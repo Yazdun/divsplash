906ee015f4f7907939ebef8ec80dcd6de813dab4
Yazdun <83041367+Yazdun@users.noreply.github.com>
2023-08-16 23:22:40 +0330

    Add Supabase (#3)
    * create basic navigation
    
    * create basic hero section
    
    * add icons
    
    * add doodle page
    
    * add radix ui
    
    * update styles
    
    * add dialogs
    
    * add toasts
    
    * improve ui
    
    * update navigation
    
    * create socials login button
    
    * add socials login
    
    * add auth to the project
    
    * fix scrollbar
    
    * add auth
    
    * add supabase
    
    * add packages

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth/next'
 import GoogleProvider from 'next-auth/providers/google'
+import { SupabaseAdapter } from '@auth/supabase-adapter'
+import { Adapter } from 'next-auth/adapters'
 
 const handler = NextAuth({
   providers: [
@@ -8,6 +10,10 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
+  }) as Adapter,
   pages: {
     signIn: '/',
   },
--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { User as UserProfile } from '@/components'
+import { createClient } from '@supabase/supabase-js'
 
-export default function User() {
+export default async function User() {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      db: {
+        schema: 'next_auth',
+      },
+    },
+  )
+
+  const { data } = await supabase.from('users').select('name')
+
   return (
-    <div className="container mt-10 text-center">
-      <UserProfile />
-    </div>
+    <div className="container mt-10 text-center">{JSON.stringify(data)}</div>
   )
 }
