ac3e534a54ecaadbacdb40eb9f2faab78bdd0f4f
Yazdun <yazduntube@gmail.com>
2023-08-26 02:24:49 +0330

    update route

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -14,5 +14,5 @@ export async function GET(request: NextRequest) {
     await supabase.auth.exchangeCodeForSession(code)
   }
 
-  return NextResponse.redirect(requestUrl.origin)
+  return NextResponse.redirect(`${requestUrl.origin}/user/profile`)
 }
