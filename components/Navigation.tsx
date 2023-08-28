feea4ef1aece1ae767922563c3769ccb41a924d7
Yazdun <yazduntube@gmail.com>
2023-08-28 22:13:44 +0330

    update navigation bar

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 import { AuthButtonServer } from './auth-button-server'
+import { TbGuitarPick } from 'react-icons/tb'
 
 export const Navigation = () => {
   return (
     <div className="sticky top-0 z-50 py-3 bg-white border-b-2 border-zinc-100">
       <div className="container relative flex items-center justify-between">
         <Link href="/" className="font-medium">
-          DivSplash
+          <TbGuitarPick className="text-2xl" />
         </Link>
 
         <AuthButtonServer />
