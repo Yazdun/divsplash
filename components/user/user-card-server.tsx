e40d4a08cfe66a4d7c732b15b34438ab92ebcbed
Yazdun <yazduntube@gmail.com>
2023-08-30 00:26:24 +0330

    create custom card component

--- components/user/user-card-server.tsx
+++ components/user/user-card-server.tsx
@@ -2,12 +2,13 @@ import { Avatar } from '@radix-ui/themes'
 import { Session } from '@supabase/supabase-js'
 import React from 'react'
 import { AuthButtonSignOut } from '../auth'
+import { Card } from '@/components/ui'
 
 export const UserCardServer = ({ session }: { session: Session | null }) => {
   if (!session) return null
 
   return (
-    <div className="flex items-center justify-center max-w-lg gap-5 p-5 bg-white border-2 rounded-md lg:max-w-sm lg:items-center lg:flex-col border-zinc-100">
+    <Card className="flex items-center justify-center max-w-lg gap-5 lg:max-w-sm lg:items-center lg:flex-col">
       <Avatar
         size="7"
         src={session.user.user_metadata.avatar_url}
@@ -20,6 +21,6 @@ export const UserCardServer = ({ session }: { session: Session | null }) => {
         </ul>
         <AuthButtonSignOut />
       </div>
-    </div>
+    </Card>
   )
 }
