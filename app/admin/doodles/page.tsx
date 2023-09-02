6438f3154969aea885cec84d8b3590bddaf15b11
Yazdun <yazduntube@gmail.com>
2023-09-02 16:05:42 +0330

    create doodles page

--- app/admin/doodles/page.tsx
+++ app/admin/doodles/page.tsx
@@ -1,9 +1,10 @@
-import { Card, DoodlesTable } from '@/components'
+import { Card, DoodlesTable, UploadDoodleDialog } from '@/components'
 import React from 'react'
 
 export default function Doodles() {
   return (
-    <div className="container text-center">
+    <div className="container flex flex-col items-end gap-5">
+      <UploadDoodleDialog />
       <DoodlesTable />
     </div>
   )
--- components/doodles/doodle-upload-dialog.tsx
+++ components/doodles/doodle-upload-dialog.tsx
@@ -2,16 +2,20 @@
 
 import React from 'react'
 import { Button, Dialog } from '@radix-ui/themes'
+import { AiOutlinePlus } from 'react-icons/ai'
 
 export const UploadDoodleDialog = () => {
   return (
     <Dialog.Root>
       <Dialog.Trigger>
-        <Button>Upload Doodle</Button>
+        <Button variant="solid" className="flex items-center gap-3">
+          <AiOutlinePlus />
+          Create Doodle
+        </Button>
       </Dialog.Trigger>
 
-      <Dialog.Content style={{ maxWidth: 600 }} className="m-5">
-        <Dialog.Title>Upload Doodle</Dialog.Title>
+      <Dialog.Content style={{ maxWidth: 600, margin: 20 }} className="m-5">
+        <Dialog.Title>Create Doodle</Dialog.Title>
       </Dialog.Content>
     </Dialog.Root>
   )
