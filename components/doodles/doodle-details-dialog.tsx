b7cab8f9c509603c3e63c105625397f6e4e6b606
Yazdun <yazduntube@gmail.com>
2023-09-02 19:41:17 +0330

    update dialog

--- components/doodles/doodle-details-dialog.tsx
+++ components/doodles/doodle-details-dialog.tsx
@@ -11,14 +11,15 @@ export const DoodleDetailsDialog = ({ doodle }: { doodle: TDoodle }) => {
     <Dialog.Root>
       <Dialog.Trigger>
         <li>
-          <button className="flex justify-center w-full p-5 bg-white border-2 rounded-md border-zinc-100">
+          <button className="flex flex-col items-center justify-center w-full gap-2 p-5 text-center bg-white border-2 rounded-md border-zinc-100">
             <Image src={doodle.fileUrl} alt="hello" width={200} height={200} />
+            <h2 className="font-bold">{doodle.title}</h2>
           </button>
         </li>
       </Dialog.Trigger>
 
       <Dialog.Content style={{ maxWidth: 400 }} className="m-5">
-        <Dialog.Title>Download Doodle</Dialog.Title>
+        <Dialog.Title>{doodle.title}</Dialog.Title>
         <Dialog.Description size="2" mb="4">
           <div className="flex justify-center w-full p-5 border-2 border-dashed rounded-md">
             <Image src={doodle.fileUrl} alt="hello" width={200} height={200} />
