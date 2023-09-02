6e2c0aadfc48a48ece06ca7738f2432a79499d3f
Yazdun <yazduntube@gmail.com>
2023-09-02 20:51:11 +0330

    remove cors config

--- next.config.js
+++ next.config.js
@@ -3,30 +3,6 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://www.divsplash.co',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,DELETE,PATCH,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ]
-  },
   images: {
     domains: ['utfs.io'],
   },
