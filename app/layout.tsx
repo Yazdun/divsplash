66beac74d9436c28002842b4daef1d26ffc31330
Yazdun <yazduntube@gmail.com>
2023-09-03 23:05:51 +0330

    update styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Theme } from '@radix-ui/themes'
 import { Toaster } from 'react-hot-toast'
 import clsx from 'clsx'
 import '@uploadthing/react/styles.css'
-import './globals.css'
 import '@radix-ui/themes/styles.css'
+import './globals.css'
 
 export const dynamic = 'force-dynamic'
 
