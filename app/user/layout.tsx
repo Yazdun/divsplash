2f9c450dea27a104423cee906b67d7608409bc08
Yazdun <yazduntube@gmail.com>
2023-09-07 16:24:05 +0330

    update user layout

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,7 +1,15 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { redirect } from 'next/navigation'
 import { cookies } from 'next/headers'
-import { Card, UserCardServer } from '@/components'
+import {
+  Card,
+  NavigationCardServer,
+  TNavigationCardServer,
+  UserCardServer,
+} from '@/components'
+import { ROUTES } from '@/constants'
+import { BsFolder, BsHeart } from 'react-icons/bs'
+import { FiDownloadCloud } from 'react-icons/fi'
 
 export const dynamic = 'force-dynamic'
 
@@ -9,6 +17,19 @@ export const metadata = {
   title: 'DivSplash — Dashboard',
   description: 'Welcome to DivSplash!',
 }
+
+const userRoutes: TNavigationCardServer[] = [
+  {
+    title: 'Downloaded doodles',
+    href: ROUTES.ADMIN.DOWNLOADS,
+    icon: FiDownloadCloud,
+  },
+  {
+    title: 'Liked Doodles',
+    href: ROUTES.ADMIN.DOODLES,
+    icon: BsHeart,
+  },
+]
 
 export default async function UserLayout({
   children,
@@ -27,12 +48,12 @@ export default async function UserLayout({
 
   return (
     <main className="container p-5 space-y-5">
-      <h1 className="text-lg font-bold text-center lg:text-left">
-        DivSplash User Panel 💻
-      </h1>
-      <div className="flex flex-col max-w-lg gap-5 m-auto lg:items-start lg:max-w-full lg:flex-row lg:m-0">
-        <Card style={{ padding: 0 }} className="w-full lg:w-[300px]">
+      {/* I had to use flex box instead of text align, because of uploadthing css has weird effect on this element! */}
+      <h1 className="text-lg font-bold">DivSplash User Panel 💻</h1>
+      <div className="max-w-lg gap-5 md:max-w-full lg:flex md:items-start">
+        <Card style={{ padding: 0 }} className="w-full lg:max-w-[250px] mb-5">
           <UserCardServer session={session} />
+          <NavigationCardServer routes={userRoutes} />
         </Card>
         {children}
       </div>
