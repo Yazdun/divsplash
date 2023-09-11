bc66ae3dd9de860cd2abf29dc5b862a9dd7b8208
Yazdun <yazduntube@gmail.com>
2023-09-12 01:44:05 +0330

    update tables

--- components/like/likes-table-server.tsx
+++ components/like/likes-table-server.tsx
@@ -14,7 +14,7 @@ export const LikesTableServer = ({
 }) => {
   return (
     <div className="w-full">
-      <Table.Root>
+      <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
