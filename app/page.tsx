ad3a7594e8b750651e8fd267f57511b7b9f37878
Yazdun <yazduntube@gmail.com>
2023-09-23 01:27:59 +0330

    change og

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,6 @@
 import { LandingHeroServer } from '@/components'
-import { Metadata } from 'next' // if using TypeScript
 
 export const dynamic = 'force-dynamic'
-
-export const metadata: Metadata = {
-  title: 'DivSplash — Homepage ',
-  description:
-    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga quibusdam itaque illum sapiente, accusantium cumque voluptatum! Quibusdam, ipsa libero. voluptatum! Quibusdam, ipsa libero.',
-  openGraph: {
-    title: 'DivSplash — Homepage',
-    description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga quibusdam itaque illum sapiente, accusantium cumque voluptatum! Quibusdam, ipsa libero. voluptatum! Quibusdam, ipsa libero.',
-    url: 'https://www.divsplash.co',
-    siteName: 'DivSplash',
-    images: [
-      {
-        url: 'https://raw.githubusercontent.com/Yazdun/yazdun.com/main/public/og.jpg',
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-}
 
 export default function Home() {
   return (
