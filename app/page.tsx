729b0554356b3c2162691f77bbc43a0cc3b08e7f
Yazdun <yazduntube@gmail.com>
2023-09-22 22:19:51 +0330

    update og

--- app/page.tsx
+++ app/page.tsx
@@ -3,44 +3,19 @@ import { Metadata } from 'next' // if using TypeScript
 
 export const dynamic = 'force-dynamic'
 
-// export const metadata: Metadata = {
-//   title: 'DivSplash — Homepage ',
-//   description:
-//     'Snag some fantastic doodles for your websites, apps or designs!',
-//   openGraph: {
-//     url: 'https://www.divsplash.co',
-//     siteName: 'DivSplash',
-
-//     images: [
-//       {
-//         url: 'https://www.divsplash.co/api/og/home',
-//         width: 1200,
-//         height: 630,
-
-//       },
-//     ],
-//     locale: 'en_US',
-//     type: 'website',
-//   },
-// }
-
 export const metadata: Metadata = {
+  title: 'DivSplash — Homepage ',
+  description:
+    'Snag some fantastic doodles for your websites, apps or designs!',
   openGraph: {
-    title: 'Next.js',
-    description: 'The React Framework for the Web',
-    url: 'https://nextjs.org',
-    siteName: 'Next.js',
+    url: 'https://www.divsplash.co',
+    siteName: 'DivSplash',
+
     images: [
       {
-        url: 'https://nextjs.org/og.png',
-        width: 800,
-        height: 600,
-      },
-      {
-        url: 'https://nextjs.org/og-alt.png',
-        width: 1800,
-        height: 1600,
-        alt: 'My custom alt',
+        url: 'https://www.divsplash.co/api/og/home',
+        width: 1200,
+        height: 630,
       },
     ],
     locale: 'en_US',
