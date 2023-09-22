a70a2581359deae9a58dbd1165a50e4a7048632d
Yazdun <yazduntube@gmail.com>
2023-09-22 21:29:34 +0330

    update homepage opengraph

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,32 @@
 import { LandingHeroServer } from '@/components'
+import { Metadata } from 'next' // if using TypeScript
 
 export const dynamic = 'force-dynamic'
+
+export const metadata: Metadata = {
+  openGraph: {
+    title: 'DivSplash',
+    description:
+      'Snag some fantastic doodles for your websites, apps or designs.',
+    url: 'https://www.divsplash.co/',
+    siteName: 'DivSplash',
+    images: [
+      {
+        url: 'https://www.divsplash.co/api/og/home',
+        width: 1200,
+        height: 630,
+      },
+      {
+        url: 'https://www.divsplash.co/api/og/home',
+        width: 1200,
+        height: 630,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+}
 
 export default function Home() {
   return (
