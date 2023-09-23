95dcc690ba289ea13dd6bfdeaaf321dae732c83f
Yazdun <yazduntube@gmail.com>
2023-09-23 03:40:13 +0330

    update generate metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,23 +8,26 @@ import { Metadata } from 'next' // if using TypeScript
 
 export const dynamic = 'force-dynamic'
 
-export const metadata: Metadata = {
-  title: 'DivSplash',
-  description:
-    'Snag some fantastic doodles for your websites, apps or designs!',
-  openGraph: {
-    url: 'https://www.divsplash.co',
-    siteName: 'DivSplash',
-    images: [
-      {
-        url: 'https://divsplash.co/api/og/home',
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
+export function generateMetadata(): Metadata {
+  return {
+    title: 'DivSplash',
+    description:
+      'Snag some fantastic doodles for your websites, apps or designs!',
+
+    openGraph: {
+      url: 'https://www.divsplash.co',
+      siteName: 'DivSplash',
+      images: [
+        {
+          url: 'https://divsplash.co/api/og/home',
+          width: 1200,
+          height: 630,
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+    },
+  }
 }
 
 export default function RootLayout({
